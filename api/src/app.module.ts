71aba689de37063ca1bf9cd8a46db37237c5e5e2
Boaz Marube <boazmarube1@gmail.com>
2025-10-16 09:44:49 +0300

    Added auth guard to the work

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,15 +3,38 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [config],
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (config) => ({
+        secret: config.get('jwt.secret') || 'fallback-secret-key',
+      }),
+      global: true,
+      inject: [ConfigService],
+    }),
     AuthModule,
     MongooseModule.forRootAsync({
-      useFactory: () => ({
-        uri: process.env.MONGODB_URI,
+      imports: [ConfigModule],
+      useFactory: async (config) => ({
+        uri:
+          config.get('database.connectionString') ||
+          'mongodb://localhost:27017/fixmate',
       }),
-    })
+      inject: [ConfigService],
+    }),
+    // UserModule,
+    // PrismaModule,
+    //
   ],
   controllers: [AppController],
   providers: [AppService],
