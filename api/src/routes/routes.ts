bdea69e2b29305ef20f23137c446b6d2ca5d10ca
Boaz Marube <boazmarube1@gmail.com>
2025-10-17 15:51:04 +0300

    Added user logic (get,delete,update profile)

--- api/src/routes/routes.ts
+++ api/src/routes/routes.ts
@@ -1,15 +1,18 @@
-import { Express, Request, Response, NextFunction } from "express";
+import { Express } from "express";
 import authRoutes from './authRoutes';
+import userRoutes from './userRoutes';
 
 export const registerRoutes = (app: Express) : void => {
-  app.use('/api/auth', authRoutes);
-
-   // Health check
-   app.get("/health", (_, res) => {
+  // Health check
+  app.get("/health", (_, res) => {
     res.json({
       success: true,
       message: "FixMate-lite API is running...",
       timestamp: new Date().toISOString()
     });
   });
+
+  // Routes
+  app.use('/api/auth', authRoutes);
+  app.use('/api/users', userRoutes);
 }
